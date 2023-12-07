83895464e49c0ed07f77d47af20711a9fff4766d
trucBel <bellion.aptx@gmail.com>
2023-12-07 11:47:05 +0700

    pandosima

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,16 +1,21 @@
-import datetime
-
 from django.db import models
+import datetime
 from django.utils import timezone
-
+from django.contrib import admin
+# Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=201)
     pub_date = models.DateTimeField("date published")
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     def __str__(self):
         return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -18,3 +23,7 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from . import views
-
 app_name = "polls"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
